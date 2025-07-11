b696213ed6e18299cd4ba81b4598c56b3748d220
sgururaj2021 <gururaj1931@gmail.com>
2025-07-12 00:18:59 +0530

    modified changes in cart.sh

--- cart.sh
+++ cart.sh
@@ -1,20 +1,3 @@
-Nodejs() {
-    dnf module disable nodejs -y
-    dnf module enable nodejs:20 -y
-    dnf install nodejs -y
-    
-    rm -rf /app
-    cp ${component}.service /etc/systemd/system/${component}.service
-    
-    useradd roboshop
-    mkdir /app 
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
-    cd /app 
-    unzip /tmp/${component}.zip
-    cd /app 
-    npm install 
-
-    systemctl daemon-reload
-    systemctl enable ${component} 
-    systemctl start ${component}
-}
+source import_func.sh
+component=cart
+Nodejs
